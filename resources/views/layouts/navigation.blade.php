2e9b99ff1118e6a1d1174bce14724abf830e9648
KanataOkumura <okumura.kanata.971@s.kyushu-u.ac.jp>
2024-10-19 15:15:07 +0900

    マージ後バグ修正

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,40 +1,25 @@
 <nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
-
-    <!-- Primary Navigation Menu -->
+   <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                      <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                      {{ __('Dashboard') }}
                     </x-nav-link>
                     <x-nav-link :href="route('categories.index')" :active="request()->routeIs('categories.index')">
-                    {{ __('カテゴリ選択') }}
+                     {{ __('カテゴリ選択') }}
                     </x-nav-link>
                 </div>
-            </div>
-
-        <!-- Navigation Links -->
-        <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-          <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-            {{ __('Dashboard') }}
-          </x-nav-link>
-        </div>
-        <!-- Map Links -->
-        <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">	
-          <x-nav-link :href="url('/leaflet')" :active="request()->is('leaflet')">	
-            {{ __('Leaflet Map') }}	
-          </x-nav-link>	
-        </div>
-      </div>
+              </div>
       
     <!-- Settings Dropdown -->
       <div class="hidden sm:flex sm:items-center sm:ms-6">
