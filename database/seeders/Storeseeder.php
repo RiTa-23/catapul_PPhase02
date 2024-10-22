8ff5b2c2716db64e011dc9ef7bd902841ba73d91
KanataOkumura <okumura.kanata.971@s.kyushu-u.ac.jp>
2024-10-22 21:21:07 +0900

    更新

--- database/seeders/Storeseeder.php
+++ database/seeders/Storeseeder.php
@@ -4,14 +4,3 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
-class Storeseeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        //
-    }
-}
